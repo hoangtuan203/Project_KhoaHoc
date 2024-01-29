72bb7c8026ab8ea76e7dd55cc03834cfc992e15d
nguyenhoangtuan12102003 <116452243+nguyenhoangtuan12102003@users.noreply.github.com>
2024-01-29 16:51:38 +0700

    upadate file DatabaseConnect.jav

---
+++ src/main/java/DAL/DatabaseConnect.java
@@ -0,0 +1,59 @@
+package DAL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class DatabaseConnect {
+   private static Connection c;
+    private static Statement s;
+    
+    private static String host, port, dbName, dbUser, dbPassword;
+    
+    
+    public static void connectDB()
+    {
+        host = "localhost";
+        port = "3306";
+        dbUser = "root";
+        dbName = "course_management";
+        dbPassword = "";
+        String dbPath = "jdbc:mysql://" + host + ":" + port + "/"
+                + dbName + "?useUnicode=yes&characterEncoding=UTF-8";
+        try {
+            c = (Connection) DriverManager.getConnection(dbPath, dbUser, dbPassword);
+            s = c.createStatement();
+            
+            System.out.println("Kết nối thành công DB");
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+    public static Connection getConnection()
+    {
+        return c;
+    }
+    //run sql
+    public static ResultSet doReadQuery(String sql) {
+        ResultSet rs = null;
+        
+        try {
+            rs = s.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnect.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    
+
+    //test connection
+    public static void main(String[] args) {
+        DatabaseConnect.connectDB();
+        
+    } 
+}
