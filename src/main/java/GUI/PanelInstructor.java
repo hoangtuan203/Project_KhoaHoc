c0b834aa0c89a33656d5754fa937a21a28937311
Vo Cong Anh <anhvca1234@gmail.com>
2024-02-28 13:05:49 +0700

    update CourseInstructor

--- src/main/java/GUI/PanelInstructor.java
+++ src/main/java/GUI/PanelInstructor.java
@@ -72,6 +72,7 @@ public class PanelInstructor extends javax.swing.JPanel {
         jLabel9 = new javax.swing.JLabel();
         btnSave = new javax.swing.JButton();
         btnReload = new javax.swing.JButton();
+        btnDetailsView = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(990, 660));
         setLayout(new java.awt.GridLayout(1, 0));
@@ -167,6 +168,13 @@ public class PanelInstructor extends javax.swing.JPanel {
             }
         });
 
+        btnDetailsView.setText("Xem chi tiết");
+        btnDetailsView.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDetailsViewActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
@@ -189,7 +197,8 @@ public class PanelInstructor extends javax.swing.JPanel {
                         .addGap(126, 126, 126)
                         .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btnSave, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnReload, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE))))
+                            .addComponent(btnReload, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+                            .addComponent(btnDetailsView, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
@@ -207,9 +216,11 @@ public class PanelInstructor extends javax.swing.JPanel {
                     .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(37, 37, 37)
                 .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnReload, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(82, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnDetailsView, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
         );
 
         jPanel6.add(jPanel3);
@@ -218,7 +229,7 @@ public class PanelInstructor extends javax.swing.JPanel {
         add(jPanel6);
     }// </editor-fold>//GEN-END:initComponents
 
-    
+
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         int i = Tablephancong.getSelectedRow();
         if (i >= 0) {
@@ -242,7 +253,7 @@ public class PanelInstructor extends javax.swing.JPanel {
                     JOptionPane.showMessageDialog(this, "Phân công giảng dạy thành công");
                 } else if (IDcourse != IDcoursechange && IDperson != IDpersonchange) {
                     JOptionPane.showMessageDialog(this, "Chỉ thay đổi 1 trường : tên giáo viên hoặc tên môn học");
-                } 
+                }
             } else {
                 int IDperson = Integer.parseInt(Tablephancong.getValueAt(i, 1).toString());
                 int IDcourse = Integer.parseInt(Tablephancong.getValueAt(i, 3).toString());
@@ -285,6 +296,23 @@ public class PanelInstructor extends javax.swing.JPanel {
         jblSearch.setText("");
         displayCourseInstructors();
     }//GEN-LAST:event_btnReloadActionPerformed
+
+    private void btnDetailsViewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDetailsViewActionPerformed
+         int selectedRow = Tablephancong.getSelectedRow();
+
+    if (selectedRow != -1) {
+        int personID = Integer.parseInt(Tablephancong.getValueAt(selectedRow, 1).toString());
+        String personName = (String) Tablephancong.getValueAt(selectedRow, 2);
+        int courseID = Integer.parseInt(Tablephancong.getValueAt(selectedRow, 3).toString());
+        String title = (String) Tablephancong.getValueAt(selectedRow, 4);
+
+    
+        String message = String.format("ID giảng viên: %d\nTên giảng viên: %s\nID khóa học: %d\nTên khóa học: %s", personID, personName, courseID, title);
+        JOptionPane.showMessageDialog(this, message, "Chi tiết", JOptionPane.INFORMATION_MESSAGE);
+    } else {
+        JOptionPane.showMessageDialog(this, "Vui lòng chọn vào table để xem chi tiết", "Thông báo", JOptionPane.WARNING_MESSAGE);
+    }
+    }//GEN-LAST:event_btnDetailsViewActionPerformed
     private void displaySearchResult(List<CourseInstructorDTO> courseInstructors) {
         DefaultTableModel model = (DefaultTableModel) Tablephancong.getModel();
         model.setRowCount(0);
@@ -351,6 +379,7 @@ public class PanelInstructor extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable Tablephancong;
+    private javax.swing.JButton btnDetailsView;
     private javax.swing.JButton btnReload;
     private javax.swing.JButton btnSave;
     private javax.swing.JButton btnSearch;
